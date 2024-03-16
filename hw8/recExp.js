1151c7f7ab9075710396ff6c15ab094e2b1c42a1
Owm <owmaslak@gmail.com>
2024-03-16 17:49:05 +0200

    add inputs

--- hw8/recExp.js
+++ hw8/recExp.js
@@ -1,7 +1,23 @@
-const linkHWItem_1 = document.getElementById(`hw8_1`)
-linkHWItem_1.addEventListener("click", () => console.log(pow(3, 4)))
+var inputNumber = document.querySelector('input[name="number"]')
+var inputDegree = document.querySelector('input[name="degree"]')
+
+const linkHWItem_1 = document.getElementById("hw8_1")
+linkHWItem_1.addEventListener("click", () =>
+  console.log(
+    "Result:",
+    pow(Number(inputNumber.value), Number(inputDegree.value))
+  )
+)
 
 const pow = (num, degree) => {
+  if (inputNumber.value.length === 0 || inputDegree.value.length === 0) {
+    console.log("One of the fields is EMPTY")
+    return NaN
+  }
+  if (isNaN(inputNumber.value) || isNaN(inputDegree.value)) {
+    console.log("One of the fields is NOT A NUMBER")
+    return NaN
+  }
   if (degree === 0) return 1
   else if (degree < 0) {
     return 1 / pow(num, -degree)
