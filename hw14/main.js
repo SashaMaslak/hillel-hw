31d64f7aa013596a5f2da331dc4b2580ee8b0b3a
Owm <owmaslak@gmail.com>
2024-04-01 22:35:39 +0300

    add feature hw14

--- hw14/main.js
+++ hw14/main.js
@@ -1,7 +1,5 @@
 const detailsBlock = document.getElementById("details")
-console.log(`detailsBlock:`, detailsBlock)
 const completedBlock = document.getElementById("completed")
-console.log(`completedBlock:`, completedBlock)
 
 document.querySelector(".btn-buy").addEventListener("click", function () {
   detailsBlock.classList.add("show")
@@ -11,6 +9,22 @@ document.querySelector(".btn-buy").addEventListener("click", function () {
   })
 })
 
+document.querySelector(".btn-cancel").addEventListener("click", function () {
+  detailsBlock.classList.remove("show")
+})
+
 document.querySelector(".btn-confirm").addEventListener("click", function () {
   completedBlock.classList.add("show")
+  window.scrollBy({
+    top: 200,
+    behavior: "smooth",
+  })
+})
+
+document.querySelector(".btn-confirm").addEventListener("submit", function () {
+  completedBlock.classList.add("show")
+  window.scrollBy({
+    top: 200,
+    behavior: "smooth",
+  })
 })
