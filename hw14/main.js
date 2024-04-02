fa9ac0100bf5584a2b480351e5cb424d50145903
Owm <owmaslak@gmail.com>
2024-04-02 23:08:22 +0300

    add hw14

--- hw14/main.js
+++ hw14/main.js
@@ -1,5 +1,10 @@
+const form = document
+  .getElementById("form")
+  .addEventListener("submit", submitForm)
 const detailsBlock = document.getElementById("details")
 const completedBlock = document.getElementById("completed")
+
+const formObj = {}
 
 document.querySelector(".btn-buy").addEventListener("click", function () {
   detailsBlock.classList.add("show")
@@ -28,3 +33,31 @@ document.querySelector(".btn-confirm").addEventListener("submit", function () {
     behavior: "smooth",
   })
 })
+
+function submitForm(event) {
+  event.preventDefault()
+
+  const formData = new FormData(this)
+
+  formData.forEach((value, key) => (formObj[key] = value))
+
+  console.log(formObj)
+  const NameField = (document.querySelector(
+    ".nameField"
+  ).textContent = `${formObj.name}`)
+  const cityField = (document.querySelector(
+    ".cityField"
+  ).textContent = `${formObj.city}`)
+  const deliveryField = (document.querySelector(
+    ".deliveryField"
+  ).textContent = `${formObj.delivery}`)
+  const payMethodField = (document.querySelector(
+    ".payMethodField"
+  ).textContent = `${formObj.payMethod}`)
+  const countProductsField = (document.querySelector(
+    ".countProductsField"
+  ).textContent = `${formObj.countProducts}`)
+  const commentField = (document.querySelector(
+    ".commentField"
+  ).textContent = `${formObj.comment}`)
+}
