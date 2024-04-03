33fbb708739c86c5e9b705bf66680b62048df8e9
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2024-04-03 16:57:20 +0300

    hw14 completed

--- hw14/main.js
+++ hw14/main.js
@@ -9,7 +9,7 @@ const formObj = {}
 document.querySelector(".btn-buy").addEventListener("click", function () {
   detailsBlock.classList.add("show")
   window.scrollBy({
-    top: 100,
+    top: 300,
     behavior: "smooth",
   })
 })
@@ -18,46 +18,26 @@ document.querySelector(".btn-cancel").addEventListener("click", function () {
   detailsBlock.classList.remove("show")
 })
 
-document.querySelector(".btn-confirm").addEventListener("click", function () {
-  completedBlock.classList.add("show")
-  window.scrollBy({
-    top: 200,
-    behavior: "smooth",
-  })
-})
-
-document.querySelector(".btn-confirm").addEventListener("submit", function () {
-  completedBlock.classList.add("show")
-  window.scrollBy({
-    top: 200,
-    behavior: "smooth",
-  })
-})
-
 function submitForm(event) {
   event.preventDefault()
 
   const formData = new FormData(this)
-
   formData.forEach((value, key) => (formObj[key] = value))
 
   console.log(formObj)
-  const NameField = (document.querySelector(
-    ".nameField"
-  ).textContent = `${formObj.name}`)
-  const cityField = (document.querySelector(
-    ".cityField"
-  ).textContent = `${formObj.city}`)
-  const deliveryField = (document.querySelector(
-    ".deliveryField"
-  ).textContent = `${formObj.delivery}`)
-  const payMethodField = (document.querySelector(
-    ".payMethodField"
-  ).textContent = `${formObj.payMethod}`)
-  const countProductsField = (document.querySelector(
+
+  document.querySelector(".nameField").textContent = `${formObj.name}`
+  document.querySelector(".cityField").textContent = `${formObj.city}`
+  document.querySelector(".deliveryField").textContent = `${formObj.delivery}`
+  document.querySelector(".payMethodField").textContent = `${formObj.payMethod}`
+  document.querySelector(
     ".countProductsField"
-  ).textContent = `${formObj.countProducts}`)
-  const commentField = (document.querySelector(
-    ".commentField"
-  ).textContent = `${formObj.comment}`)
+  ).textContent = `${formObj.countProducts}`
+  document.querySelector(".commentField").textContent = `${formObj.comment}`
+
+  completedBlock.classList.add("show")
+  window.scrollBy({
+    top: 300,
+    behavior: "smooth",
+  })
 }
