3a5d52b8c5fbf972bb7687489d3ad13febe92d9a
Owm <owmaslak@gmail.com>
2024-04-24 22:30:45 +0300

    in progress

--- hw18/index.js
+++ hw18/index.js
@@ -1,16 +1,42 @@
-async function fetchData() {
-  try {
-    const response = await fetch("https://api.example.com/data")
-    if (!response.ok) {
-      throw new Error("Network response was not ok")
+fetch(
+  "http://api.openweathermap.org/data/2.5/weather?q=kyiv&units=metric&APPID=5d066958a60d315387d9492393935c19"
+)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    const weather = {
+      city: document.querySelector("#city"),
+      country: document.querySelector("#country"),
+      weatherIcon: document.querySelector("#weather-icon"),
+      weatherDescription: document.querySelector("#weather-description"),
+      temperature: document.querySelector("#temperature"),
+      windSpeed: document.querySelector("#wind-speed"),
+      windDeg: document.querySelector("#wind-deg"),
+      humidity: document.querySelector("#humidity"),
+      pressure: document.querySelector("#pressure"),
     }
-    const data = await response.json() // Парсимо JSON-відповідь
-    console.log("Отримані дані:", data)
-    // Обробляємо дані в поточному синхронному коді
-  } catch (error) {
-    console.error("Виникла помилка під час виконання запиту:", error)
-  }
-}
 
-// Викликаємо функцію для виконання запиту та обробки отриманих даних
-fetchData()
+    weather.city.textContent = data.name
+    weather.country.textContent = data.sys.country
+
+    weather.weatherDescription.textContent = data.weather[0].description
+    weather.temperature.textContent = data.main.temp
+    weather.windSpeed.textContent = `${data.wind.speed} m/sec`
+    weather.windDeg.textContent = `${data.wind.deg}°`
+    weather.humidity.textContent = `${data.main.humidity}%`
+    weather.pressure.textContent = `${data.main.pressure} mm`
+    weather.weatherIcon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    )
+  })
+
+// const city = document.querySelector("#city")
+// const country = document.querySelector("#country")
+// const weatherIcon = document.querySelector("#weather-icon")
+// const weatherDescription = document.querySelector("#weather-description")
+// const temperature = document.querySelector("#temperature")
+// const windSpeed = document.querySelector("#wind-speed")
+// const windDir = document.querySelector("#wind-dir")
+// const humidity = document.querySelector("#humidity")
+// const pressure = document.querySelector("#pressure")
