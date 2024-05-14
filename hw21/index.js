2a59081edb486627e5f358aca192623133a7b91d
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2024-05-14 08:34:52 +0300

    hw21 done

--- hw21/index.js
+++ hw21/index.js
@@ -1,34 +1,22 @@
-function* Foo() {
+function Boo() {
+  let count = 0
   console.log("start")
-  yield 1
-  yield 2
-  yield 3
-  console.log("finish")
+
+  return {
+    next: function () {
+      if (count <= 2) {
+        count++
+        return { value: count, done: false }
+      } else {
+        console.log("finish")
+        return { done: true }
+      }
+    },
+  }
 }
 
-const iterator = Foo()
-iterator.next().value // 1
-iterator.next().done // false
-
-// function Boo() {
-//   let count = 0;
-//   console.log('start');
-
-//   return {
-//     next: function() {
-//       if (count <= 2) {
-//         count++;
-//         return { value: count, done: false };
-//       } else {
-//         console.log('finish');
-//         return { done: true };
-//       }
-//     }
-//   };
-// }
-
-// const iterator = Boo();
-// console.log(iterator.next().value); // 1
-// console.log(iterator.next().value); // 2
-// console.log(iterator.next().value); // 3
-// console.log(iterator.next().done); // true
+const iterator = Boo()
+console.log(iterator.next().value) // 1
+console.log(iterator.next().value) // 2
+console.log(iterator.next().value) // 3
+console.log(iterator.next().done) // true
